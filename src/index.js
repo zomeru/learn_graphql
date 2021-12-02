c797e5193d0e737a69b752e2bdef989ebde68faa
Zomer <zomerzxc.19@gmail.com>
2021-12-03 00:44:59 +0800

    test query with arguments

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import axios from 'axios';
 const server = new GraphQLServer({
   typeDefs: `
     type Query {
-      post: Post!
+      post(id: ID!): Post!
       posts: [Post!]
+      multiply(value: Int!): Int
     }
 
      type Post {
@@ -17,16 +18,20 @@ const server = new GraphQLServer({
   `,
   resolvers: {
     Query: {
-      post: async () => {
-        const response = await axios.get('http://localhost:3004/posts/1');
+      post: async (parent, args, context, info) => {
+        const response = await axios.get(
+          `http://localhost:3004/posts/${args.id}`
+        );
 
         return response.data;
       },
-
       posts: async () => {
         const response = await axios.get('http://localhost:3004/posts');
 
         return response.data;
+      },
+      multiply: async (parent, args, context, info) => {
+        return args.value * 10;
       },
     },
   },
